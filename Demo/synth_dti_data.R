0c856f5059baba8b032966dce3c605de1a133254
Matthew Parker <mrparker909@gmail.com>
2019-06-07 15:15:26 -0700

    working on demo

--- Demo/synth_dti_data.R
+++ Demo/synth_dti_data.R
@@ -11,34 +11,34 @@ X = rbind(X1, X2)#[X1;X2];
 npivots = sizeR(X,1) # Number of points except the base point
 Yp = array(0, dim=c(3,3,npivots+1))#zeros(3,3,npivots+1);
 
-Yp[,,1] = randspd(3,2,3);
-Yp(:,:,2) = randspd(3,2,10);
-Yp(:,:,3) = randspd(3,2,10);
+Yp[,,1] = randspd(3,2,3)
+Yp[,,2] = randspd(3,2,10)
+Yp[,,3] = randspd(3,2,10)
 
-% Tangent vectors, geodesic bases.
-V = zeros(3,3,npivots);
-for j =1:npivots
-V(:,:,j) = logmap_spd(Yp(:,:,1),Yp(:,:,j+1));
-end
+# Tangent vectors, geodesic bases.
+V = array(0, dim=c(3,3,npivots)) #zeros(3,3,npivots);
+for(j in 1:npivots) {
+  V[,,j] = logmap_spd(Yp[,,1], Yp[,,j+1])
+}
 
-%% Generate Ground Truth Data
-Y = zeros(3,3,size(X,2));
-for i = 1:size(X,2)
-Vtmp = zeros(3,3,1);
-for j =1:npivots
-Vtmp = Vtmp+ V(:,:,j)*X(j,i);
-end
-Y(:,:,i) = expmap_spd(Yp(:,:,1),Vtmp);
-end
+# Generate Ground Truth Data
+Y = array(0, dim=c(3,3, sizeR(X,2))) #zeros(3,3,size(X,2));
+for(i in 1:sizeR(X,2)) {
+  Vtmp = array(0, dim=c(3,3,1))  #zeros(3,3,1)
+  for(j in 1:npivots) {
+    Vtmp = Vtmp + V[,,j]%*%X[j,i]
+  }
+  Y[,,i] = expmap_spd(Yp[,,1],Vtmp)
+}
 
-%% Sanity check
-notspd = 0 ;
-for i=1:size(Y,3)
-notspd = notspd + (~isspd(Y(:,:,i)));
-end
-assert(notspd ==0)
-%%
-  Xsample = [];
+# Sanity check
+notspd = 0
+for(i in 1:sizeR(Y,3)) {
+  notspd = notspd + (!isspd(Y[,,i]))
+}
+if(notspd !=0) stop("generated Y is not an spd!")
+#
+Xsample = [];
 Ysample = zeros(3,3,size(Y,3)*npairs);
 isample = 1;
 for i = 1:npairs
